58a6e73e9fe735b8fc470dc44fd07caabfc13ee3
Matheus Kuster <matheuskuster@hotmail.com>
2019-04-17 19:54:05 -0300

    Deploy

---
+++ src/pages/Box/index.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import { MdInsertDriveFile } from "react-icons/md";
+import { distanceInWords } from "date-fns";
+import pt from "date-fns/locale/pt";
+import Dropzone from "react-dropzone";
+import socket from "socket.io-client";
+
+import "./styles.css";
+import logo from "../../assets/logo.svg";
+import api from "../../services/api";
+
+export default class Box extends Component {
+  state = {
+    box: {}
+  };
+
+  async componentDidMount() {
+    this.subscribeToNewFiles();
+
+    const box = this.props.match.params.id;
+    const response = await api.get(`box/show/${box}`);
+
+    this.setState({ box: response.data });
+  }
+
+  subscribeToNewFiles = () => {
+    const box = this.props.match.params.id;
+    const io = socket("https://kusterbox-backend.herokuapp.com");
+
+    io.emit("connectRoom", box);
+
+    io.on("file", data => {
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box.files] }
+      });
+    });
+  };
+
+  handleUpload = files => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+
+      data.append("file", file);
+
+      api.post(`box/${box}/files`, data);
+    });
+  };
+
+  render() {
+    return (
+      <div id="box-container">
+        <header>
+          <div id="logo">
+            <img src={logo} alt="" />
+            <p id="logo-text">KusterBox</p>
+            <h1>{this.state.box.title}</h1>
+          </div>
+        </header>
+
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
+
+              <p>Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
+
+        <ul>
+          {this.state.box.files &&
+            this.state.box.files.map(file => (
+              <li key={file._id}>
+                <a className="fileInfo" href={file.url} target="_blank">
+                  <MdInsertDriveFile size={24} color="#A5Cfff" />
+                  <strong>{file.title}</strong>
+                </a>
+
+                <span>
+                  há{" "}
+                  {distanceInWords(file.createdAt, new Date(), {
+                    locale: pt
+                  })}
+                </span>
+              </li>
+            ))}
+        </ul>
+      </div>
+    );
+  }
+}
